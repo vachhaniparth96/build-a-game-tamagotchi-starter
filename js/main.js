ef45d8054adbe266de219b2ae901a427726346b2
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-10-27 14:20:48 -0400

    Added button event handlers

--- js/main.js
+++ js/main.js
@@ -28,28 +28,94 @@ const boredomStatEl = document.getElementById('boredom-stat'); //can also use qu
 const hungerStatEl = document.getElementById('hunger-stat');
 const tiredStatEl = document.getElementById('sleepiness-stat');
 
+// const startButton = document.getElementById('start');
 const gameButtonEl = document.querySelectorAll('#button-display button');
 
 /*----- event listeners -----*/
+gameButtonEl.forEach(function(btn) {
+    btn.addEventListener('click', handleClick)
+})
+function handleClick(e){
+    console.log(e.target.innerHTML)
 
+    const convertName = {
+        feed: "hunger",
+        sleep: "sleepiness",
+        play: "boredom"
+    }
+
+    const key = convertName[e.target.innerText];
+    console.log(key)
+
+    updateStat(key, -3);
+
+    render();
+}
 
 /*----- functions -----*/
 init ();
 
 // Initialize all states, then call render()
 function init() {
-    state = {...INIT_DATA}
-    age=0;
-    cycles=0;
-    interval=5000;
-    timer = setInterval(runGame, interval)
-    console.log('game started!');
-    render();
+    // if(e.target.innerHTML === 'start'){
+        state = {...INIT_DATA}
+        age=0;
+        cycles=0;
+        interval=5000;
+        timer = setInterval(runGame, interval)
+        console.log('game started!');
+        render()
+    // }
 }
 
 function runGame() {
     // console.log('Game is running');
+    cycles++;
+
+    if(continueGame()) {
+        updateStats();
+    } else {
+        gameOver();
+    }
+    render();
 }
 function render() {
     // console.log('rendering game');
+    renderStats();
 }
+
+function renderStats() {
+    boredomStatEl.textContent=state.boredom
+    hungerStatEl.textContent=state.hunger
+    tiredStatEl.textContent=state.sleepiness
+}
+
+function updateStats() {
+    // let currentValue = state[key]
+    for(key in state) {
+        // let randomUpdate = Math.floor(Math.random()*3)
+        // state[key] = currentValue + randomUpdate;
+        // console.log(key, state[key], randomUpdate)
+    updateStat(key, Math.floor(Math.random()*3))
+    }
+}
+
+function updateStat(stat,value) {
+    if(state[stat] + value >= 0){
+    state[stat]+= value
+    } else {
+        state[stat] = 0;
+    }
+}
+
+function continueGame() { //Check all properteies of each state, evaluate if any properties reach 10. If not, continue game, if so, game over.
+    const testGame = Object.values(state).every((stat) => stat < 10);
+    return testGame;
+}
+
+function gameOver() {
+    alert('Game Over!')
+    clearInterval(timer)
+    init();
+}
+
